afd76eb6cfc15566df84466b739328937fd3740b
ajaykanojiya <ajaykanojiya2205@gmail.com>
2025-10-01 18:01:53 +0530

    Code update -1.3 Leet code: Egnyte Maximum sum of subarray

---
+++ src/leetcode/MaximumSumSubArray.java
@@ -0,0 +1,29 @@
+package leetcode;
+/*
+* Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+* Example 2:
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.*/
+public class MaximumSumSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println("Max Sum of Sub Array: "+maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int [] nums){
+        int currSum = 0, maxSum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(currSum < 0)
+                currSum = nums[i];
+            else
+                currSum = currSum + nums[i];
+            maxSum = Math.max(maxSum,currSum);
+        }
+        return maxSum;
+    }
+}
