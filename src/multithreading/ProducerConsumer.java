d7c3dc209bd50f9356e4b77aa8dd608681899ae1
ajaykanojiya <ajaykanojiya2205@gmail.com>
2025-10-09 21:45:16 +0530

    Code update -1.10 Multithreading: ProducerConsumer efficient program

--- src/multithreading/ProducerConsumer.java
+++ src/multithreading/ProducerConsumer.java
@@ -1,71 +1,81 @@
 package multithreading;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ProducerConsumer {
 
-    List<Integer> QUEUE = new ArrayList<>();
-    final int SIZE = 10;
+    List<Integer> list = new LinkedList<>();
+    final int LIMIT = 10;
+    private static AtomicBoolean stop = new AtomicBoolean(true);
 
     public static void main(String[] args) {
         ProducerConsumer producerConsumer = new ProducerConsumer();
-        Thread producer = new Thread(()-> producerConsumer.producer());
-        Thread consumer = new Thread(()-> producerConsumer.consumer());
+        Thread producer = new Thread(producerConsumer::produce);
+        Thread consumer = new Thread(producerConsumer::consumer);
 
         producer.start();
         consumer.start();
-
-        System.out.println("Main thread exiting...");
-
-    }
-
-    private void producer(){
-        int number;
-        while(true){
-            synchronized (this){
-                while(QUEUE.size()>=10){
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                number = new Random().nextInt(50);
-                QUEUE.add(number);
-                System.out.println("Produced: "+number);
-                sleep(500);
-                notifyAll();
-            }
-        }
-    }
-    private void consumer(){
-        int number;
-        while(true){
-            synchronized (this){
-                while(QUEUE.size()==0){
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                number = QUEUE.getFirst();
-                QUEUE.removeFirst();
-                System.out.println("Consumed: "+number);
-                sleep(500);
-                notifyAll();
-            }
-        }
-    }
-
-    private void sleep(long millisecond){
+        System.out.println("Press enter key to stop the program...");
         try {
-            Thread.sleep(millisecond);
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stop.set(false);
+        System.out.println("Main Exit..");
+    }
+
+    public void produce(){
+        int number;
+        Random random = new Random();
+        while(stop.get()){
+            synchronized (this){
+                while(list.size() == LIMIT) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        notifyAll();
+                        throw new RuntimeException(e);
+                    }
+                }
+                number = random.nextInt(100);
+                list.add(number);
+                System.out.println("Produced: "+number);
+                sleep(1000);
+                notifyAll();
+            }
+        }
+    }
+
+    public void consumer(){
+        int number;
+        while(stop.get()){
+            synchronized (this){
+                while(list.size() == 0){
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        notifyAll();
+                        throw new RuntimeException(e);
+                    }
+                }
+                number = list.removeFirst();
+                System.out.println("Consumed: "+number);
+                sleep(1000);
+                notifyAll();
+            }
+        }
+    }
+
+    private void sleep(long time){
+        try {
+            Thread.sleep(time);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
